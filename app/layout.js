043dc8d08b3ef20180d3f2333a395ef6b21bb80b
ttstock <ttstock@gmail.com>
2026-05-14 11:05:00 +0900

    update next.js version

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+<<<<<<< Updated upstream
       <body>{children}</body>
     </html>
   );
 }
+=======
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
+>>>>>>> Stashed changes
